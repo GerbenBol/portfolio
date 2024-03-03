cffa4b69e5fbf2c681be26e45ca446cd574588a5
Gerben Bol <gerben.bol249@gmail.com>
2024-03-04 00:56:07 +0100

    gif option add

--- js/project.js
+++ js/project.js
@@ -15,19 +15,26 @@ function StoreProject(json) {
 function FillSite() {
     document.getElementsByTagName("title")[0].innerHTML = project.name + " - Gerben Bol";
     document.getElementById("proj-name").innerHTML = project.name;
-    document.getElementsByTagName("video")[0].innerHTML = "<source src='assets/vid/" + project.vid + "'>";
+    let vidgif = document.getElementById("vidgif");
+
+    if (project.vid != "") {
+        vidgif.innerHTML = "<video width='700' controls><source src='assets/vid/" + project.vid + "'></video>";
+    } else if (project.gif != "") {
+        vidgif.innerHTML = "<img width='700' src='assets/vid/" + project.gif + "'>";
+    }
+    
     let intro = document.getElementById("intro");
     let desc = document.getElementById("desc");
 
     if (project.intro == "") {
-        intro.innerHTML = "<img src='assets/img/projects/" + project.img + "' width=300>";
+        intro.innerHTML = "<img src='assets/img/projects/" + project.img + "' width=300 id='justanimg'>";
         desc.innerHTML = "<div class='text-center' id='desc-par'><p id='desc-text'>" + project.desc + "</p></div>";
     } else {
         intro.innerHTML = "<p class='text-center' id='intro-text'>" + project.intro + "</p>";
         desc.innerHTML = "<div class='col-md-6' id='desc-par'>" +
             "<p class='text-center' id='desc-text'>" + project.desc + "</p>" +
             "</div><div class='col-md-6'>" +
-            "<div><img src='assets/img/projects/" + project.img + "' width=300></div></div>";
+            "<div><img src='assets/img/projects/" + project.img + "' width=300 id='justanimg'></div></div>";
     }
 
     let introtxt = document.getElementById("intro-text");
@@ -35,12 +42,12 @@ function FillSite() {
     let desctxt = document.getElementById("desc-text");
     let descpar = document.getElementById("desc-par");
 
-    if (introtxt != undefined && introtxt != null) {
-        let iheight = introtxt.offsetHeight;
-        introtxt.style.marginTop = (intropar.offsetHeight - iheight) / 2 + "px";
-    }
-
     setTimeout(() => {
+        if (introtxt != undefined && introtxt != null) {
+            let iheight = introtxt.offsetHeight;
+            introtxt.style.marginTop = (intropar.offsetHeight - iheight) / 2 + "px";
+        }
+    
         let dheight = desctxt.offsetHeight;
         desctxt.style.marginTop = (descpar.offsetHeight - dheight) / 2 + "px";
     }, 100);
